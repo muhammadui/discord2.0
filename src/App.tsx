42707a81ea1e945abb977a105faa348e9b6c2fef
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2024-08-04 08:51:13 +0100

    feat: added chat screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import Chat from "./components/Chat"
 import Sidebar from "./components/Sidebar"
 
 const App = () => {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   )
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,9 @@
+const Chat = () => {
+  return (
+    <section className="chat__section">
+      <p>I am the chat screen component</p>
+    </section>
+  )
+}
+
+export default Chat
